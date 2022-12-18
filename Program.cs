bf132b1219a095210989f003687f506f008b1ba1
Ilia <ily5100@yandex.ru>
2022-12-18 19:38:05 +0300

    TASK 4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите первое число");
+//string numberStr = Console.ReadLine() ??"";
+int number = int.Parse(Console.ReadLine()??"0");
+if (number < 0)
+{
+    Console.WriteLine("Неверный код");
+}
+else
+{
+    int count = 1;
+    while (count < number)
+    {
+        count ++;
+        if (count%2 == 0)
+        {
+            Console.WriteLine($"{count}");
+        }
+    }
+} 
+
+
+
